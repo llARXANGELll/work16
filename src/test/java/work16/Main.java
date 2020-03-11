5c6a70b6d88fbbda26702211d287df19f29d7077
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-11 18:18:15 +0300

    master

--- src/test/java/work16/Main.java
+++ src/test/java/work16/Main.java
@@ -4,16 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
